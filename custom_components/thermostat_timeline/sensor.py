11945bccfa472bf8ef51afe940c74f0cf5603982
Glerup <k3dd3@hotmail.dk>
2025-11-07 00:46:39 +0100

    Update sensor.py

--- custom_components/thermostat_timeline/sensor.py
+++ custom_components/thermostat_timeline/sensor.py
@@ -8,8 +8,9 @@ from homeassistant.helpers.entity import DeviceInfo
 from .const import DOMAIN, SIGNAL_UPDATED
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
-    ent = ThermostatTimelineOverview(hass)
-    async_add_entities([ent], True)
+    ent_main = ThermostatTimelineOverview(hass)
+    ent_backup = ThermostatTimelineBackup(hass)
+    async_add_entities([ent_main, ent_backup], True)
 
 class ThermostatTimelineOverview(SensorEntity):
     _attr_name = "Thermostat Timeline"
@@ -46,4 +47,42 @@ class ThermostatTimelineOverview(SensorEntity):
             "schedules": self.hass.data[DOMAIN].get("schedules", {}),
             "settings": self.hass.data[DOMAIN].get("settings", {}),
             "version": int(self.hass.data[DOMAIN].get("version", 1)),
+        }
+
+
+class ThermostatTimelineBackup(SensorEntity):
+    _attr_name = "Thermostat Timeline Backup"
+    _attr_icon = "mdi:archive"
+    _attr_should_poll = False
+
+    def __init__(self, hass: HomeAssistant):
+        self.hass = hass
+        self._attr_unique_id = "thermostat_timeline_backup"
+
+    async def async_added_to_hass(self):
+        self.async_on_remove(async_dispatcher_connect(self.hass, SIGNAL_UPDATED, self._refresh))
+        await self._refresh()
+
+    async def _refresh(self):
+        self.async_write_ha_state()
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        return DeviceInfo(
+            identifiers={(DOMAIN, "thermostat_timeline")},
+            name="Thermostat Timeline",
+            manufacturer="Custom",
+        )
+
+    @property
+    def native_value(self):
+        return int(self.hass.data[DOMAIN].get("backup_version", 1))
+
+    @property
+    def extra_state_attributes(self):
+        return {
+            "schedules": self.hass.data[DOMAIN].get("backup_schedules", {}),
+            "settings": self.hass.data[DOMAIN].get("backup_settings", {}),
+            "version": int(self.hass.data[DOMAIN].get("backup_version", 1)),
+            "last_backup_ts": self.hass.data[DOMAIN].get("backup_last_ts"),
         }
