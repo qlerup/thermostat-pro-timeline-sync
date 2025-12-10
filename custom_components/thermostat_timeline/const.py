374dfcc65bee5e7e6465eb2ed92ec06238efebed
Glerup <k3dd3@hotmail.dk>
2025-12-10 23:18:24 +0100

    Remove BACKUP_SLOT_MAX constant
    Removed the maximum number of rolling backup slots constant.

--- custom_components/thermostat_timeline/const.py
+++ custom_components/thermostat_timeline/const.py
@@ -3,5 +3,3 @@ STORAGE_KEY = DOMAIN
 STORAGE_VERSION = 1
 SIGNAL_UPDATED = f"{DOMAIN}_updated"
 BACKUP_STORAGE_KEY = f"{DOMAIN}_backup"
-# Maximum number of rolling backup slots
-BACKUP_SLOT_MAX = 10
